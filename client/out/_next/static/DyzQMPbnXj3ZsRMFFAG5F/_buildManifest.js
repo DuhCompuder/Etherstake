7e4d9e485671b18aa41db3f390028b97db09a8f7
Duhcompuder <csze4107@gmail.com>
2022-01-14 15:36:52 -0500

    modified build export

---
+++ client/out/_next/static/DyzQMPbnXj3ZsRMFFAG5F/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d64684d8-8e2e678624a370e4.js","static/chunks/d7eeaac4-5549380d35918e95.js","static/chunks/pages/index-209b6dcf004df5ec.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
